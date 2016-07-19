9f24eb6ca03afb0aa4291f3c2a60da248f178603
HughChiou <hugh__@hotmail.com>
2016-07-19 16:09:30 +0800

    modified jsPrototype.js

--- DEMO/jsPrototype.js
+++ DEMO/jsPrototype.js
@@ -12,14 +12,20 @@ var p2 = new Person('Mary', 27);
 
 console.log(p1.toString());
 console.log(p2.toString());
+console.log(Person.prototype === Object.getPrototypeOf(p1));
 
-var p = new Person('Jonny', 44);
-console.log(Person.prototype === Object.getPrototypeOf(p));
 
 //Actually, the procedure is like this
-var p = {};
-p.__proto__ = Person.prototype;
-Person.call(p, 'Joy', 35);
-console.log(p.toString());
+var p3 = {};
+p3.__proto__ = Person.prototype;
+Person.call(p3, 'Joy', 35);
 
-console.log(p instanceof Person);
+console.log(p3.toString());
+console.log(p3 instanceof Person);
+
+
+var p4 = Object.create(Person.prototype);
+Person.call(p4, 'Tony', 34);
+
+console.log(p4.toString());
+console.log(p4 instanceof Person);
