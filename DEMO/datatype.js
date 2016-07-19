885fa6ab7621d92533bf0095093f6708858fea55
HughChiou <hugh__@hotmail.com>
2016-07-19 09:45:25 +0800

    finished datatype

---
+++ DEMO/datatype.js
@@ -0,0 +1,17 @@
+//Primitive data and Composite data and 2 special value-null and undefined
+function outputResult() {
+    this.forEach(function(element) {
+       console.log(element); 
+    });
+};
+
+var equalNaN = NaN == NaN;
+
+var IsNaN = isNaN(NaN);
+
+var demoNaN = isNaN(1 / "two");
+
+var ch1 = 'A';
+typeof ch1;
+var ch2 = 'B';
+typeof ch2;
